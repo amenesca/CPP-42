36bcdb1edd1062c4d636ca80edd0c0e57cd13ed0
amenesca <alanmenescal@hotmail.com>
2023-04-09 17:56:18 -0300

    começando a fazer os zombies

---
+++ cpp01/ex00/Zombie.hpp
@@ -0,0 +1,15 @@
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+class Zombie {
+
+private:
+	std::string name;
+
+public:
+	void	announce( void );
+	Zombie*	newZombie( std::string name );
+	void	randomChump( std::string name );
+};
+
+#endif

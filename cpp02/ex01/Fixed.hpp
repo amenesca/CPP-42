0617edffbcc7f69e07a5c012c9e059c1e4980af0
amenesca <alanmenescal@hotmail.com>
2023-04-25 16:03:24 -0300

    ex01 feito

--- cpp02/ex01/Fixed.hpp
+++ cpp02/ex01/Fixed.hpp
@@ -7,7 +7,7 @@
 class Fixed {
 
   private:
-	int value;
+	int fpoint;
     static const int frac_bits = 8;
   
   public:
@@ -18,9 +18,9 @@ class Fixed {
 
     float toFloat(void) const;
     int toInt(void) const;
-
     int getRawBits(void) const;
     void setRawBits(int const raw);
+    Fixed& operator=(Fixed const& fixed);
 };
 
 std::ostream& operator<<(std::ostream& os, Fixed const &Fixed);

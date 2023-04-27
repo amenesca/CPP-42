907a7f720a62fc2ebed797a3434feb0c4a695a33
amenesca <alanmenescal@hotmail.com>
2023-04-27 11:56:58 -0300

    exercio 3 no processo

--- cpp02/ex03/Point.hpp
+++ cpp02/ex03/Point.hpp
@@ -6,13 +6,18 @@
 class Point {
 
 	public:
+		Point(); // Default constructor
+		Point(float const x, float const y); // Constructor that receives float
+		Point(Point const& point); // Copy Constructor
+		~Point(); // Destructor
+		Point& operator=(Point const& point); // Copy assignment operator overload
 
-
-	
 	private:
-
-
+		Fixed const x;
+		Fixed const y;
 
 };
+
+bool bsp( Point const a, Point const b, Point const c, Point const point);
 
 #endif

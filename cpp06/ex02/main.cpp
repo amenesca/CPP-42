7480fbc5f9f58cd10e255ec0e01a84296517a909
amenesca <alanmenescal@hotmail.com>
2023-08-03 16:20:59 -0300

    ex02 main sem leak

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amenesca <amenesca@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 15:30:12 by amenesca          #+#    #+#             */
-/*   Updated: 2023/08/01 16:10:00 by amenesca         ###   ########.fr       */
+/*   Updated: 2023/08/01 16:26:59 by amenesca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,5 @@ int main()
 	Base *type = generate();
 	identify(type);
 	identify(*type);
+	delete type;
 }

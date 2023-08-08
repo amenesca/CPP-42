2fbaeaa4c6e1c89ce4e13e4e2e958cd3b332e775
amenesca <alanmenescal@hotmail.com>
2023-08-08 11:36:16 -0300

    criando template

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -1,40 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Array.tpp                                          :+:      :+:    :+:   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amenesca <amenesca@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/08 11:30:06 by amenesca          #+#    #+#             */
-/*   Updated: 2023/08/08 11:35:35 by amenesca         ###   ########.fr       */
+/*   Created: 2023/08/07 12:06:47 by amenesca          #+#    #+#             */
+/*   Updated: 2023/08/08 11:34:27 by amenesca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ARRAY_TPP
-# define ARRAY_TPP
-
-#include "Array.hpp"
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
 
 template <typename T>
-Array<T>::Array(void) {
-	_array = new T[0];
-}
-
-template <typename T>
-Array<T>::Array(unsigned int n) {
-	_array = new T[n];
-}
-
-template <typename T>
-Array<T>::~Array(void) {
-	delete[] _array;
-}
-
-template <typename T>
-Array<T>::size()  {
-	unsigned int i = 0;
-	while (_array[i++] != 0) {}
-	return (i);
-}
+class Array {
+	private:
+		T	*_array;
+	public:
+		Array(void);
+		Array(unsigned int n);
+		~Array(void);
+//		Array(const Array& copy);
+//		Array& operator=(const Array& copy);
+		unsigned int size(void);
+};
 
 #endif

ba84d028e485dba319b598aa003eaf9e76f9c42d
amenesca <alanmenescal@hotmail.com>
2023-08-14 17:11:06 -0300

    ex02 feito falta alguns detalhes

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: amenesca <amenesca@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 13:23:06 by amenesca          #+#    #+#             */
-/*   Updated: 2023/08/14 16:44:56 by amenesca         ###   ########.fr       */
+/*   Updated: 2023/08/14 17:10:35 by amenesca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ class MutantStack : public std::stack<T, std::deque<T> > {
 		
 		MutantStack(const MutantStack& copy) {
 			*this = copy;
-		
 		}
 		virtual ~MutantStack() {}
 		

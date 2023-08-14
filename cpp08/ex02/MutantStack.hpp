6cfd7228ed56c304814e18a39c94f321b0650471
amenesca <alanmenescal@hotmail.com>
2023-08-14 15:04:19 -0300

    começando ex02

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amenesca <amenesca@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/14 13:23:06 by amenesca          #+#    #+#             */
+/*   Updated: 2023/08/14 13:28:08 by amenesca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+#include <stack>
+
+template <typename T>
+class MutantStack {
+	private:
+		std::stack<T> mstack;
+	public:
+		 
+};
+
+#endif
